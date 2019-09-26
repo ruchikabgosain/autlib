f6ce2d1d76d7c7c13896ee4f863e3e5197704228
gkarlos <karlosg89n@gmail.com>
2019-09-26 23:18:35 +0200

    added write.h and write.c

---
+++ src/write/write.c
@@ -0,0 +1,16 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <aut/write.h>
+#include <aut/util/error.h>
+
+autError_t aut_write(const char *path, aut_t *aut)
+{
+  //TODO
+}
+
+#ifdef __cplusplus
+}
+#endif
