c2ca78a3ffb959e39670b575ada515e80a88fc63
gkarlos <karlosg89n@gmail.com>
2019-10-01 21:04:59 +0200

    aut_write impl and unittests

--- test/test_write_aut.cpp
+++ test/test_write_aut.cpp
@@ -1,24 +1,71 @@
+#include <gtest/gtest.h>
+
+#include <cstdio>
+
 extern "C" {
   #include <string.h>
   #include <aut.h>
   #include <aut/write.h>
 }
 
-int main(void)
+#define SMALL_FILE_1  "../../files/simple/cycle.aut"
+#define SMALL_FILE_2  "../../files/simple/serial.aut"
+#define SMALL_FILE_3  "../../files/simple/confluence.aut"
+#define SMALL_FILE_4  "../../files/simple/bintree1.aut"
+#define MEDIUM_FILE_1 "../../files/mcrl2/bus.aut"
+#define LARGE_FILE_1  "../../files/mcrl2/link1.1.aut"
+
+#define OUTFILE "outfile.aut"
+
+TEST(aut_write, invalid_arg)
 {
-  autDescriptor_t *descriptor;
-  aut_descriptor_create(&descriptor, 0, 5, 3);
+  aut_t *lts;
+  autError_t err;
+  err = aut_write(NULL, lts);
+  ASSERT_EQ(err, AUT_EINVAL1);
+  err = aut_write(OUTFILE, NULL);
+  ASSERT_EQ(err, AUT_EINVAL2);
+}
 
-  aut_t *aut;
-  aut_create(&aut, descriptor, "t");
+TEST(aut_write, valid)
+{
+  aut_t *lts1, *lts2;
+  autError_t err = aut_read(SMALL_FILE_1, &lts1);
+  EXPECT_EQ( AUT_ESUCCESS, err);
+  EXPECT_NE( AUT_NULL, lts1);
 
-  aut_add_transition(aut, 0, 1, "a");
-  aut_add_transition(aut, 0, 2, "b");
-  aut_add_transition(aut, 2, 2, "f(a)");
-  aut_add_transition(aut, 2, 1, "2->1");
-  aut_add_transition(aut, 2, 0, "2->0");
+  err = aut_write(OUTFILE, lts1);
+  EXPECT_EQ( AUT_ESUCCESS, err);
 
-  aut_write("aut_write_file.aut", aut);
+  err = aut_read(OUTFILE, &lts2);
+  EXPECT_EQ( AUT_ESUCCESS, err);
+
+  autDescriptor_t *d1 = aut_get_descriptor(lts1);
+  autDescriptor_t *d2 = aut_get_descriptor(lts2);
+  EXPECT_NE(AUT_NULL, d1);
+  EXPECT_NE(AUT_NULL, d2);
+
+  ASSERT_EQ(aut_descriptor_get_initstate(d1), aut_descriptor_get_initstate(d2));
+  ASSERT_EQ(aut_descriptor_get_num_transitions(d1), aut_descriptor_get_num_transitions(d2));
+  ASSERT_EQ(aut_descriptor_get_num_states(d1), aut_descriptor_get_num_states(d2));
+
+  autTransition_t *t1, *t2;
+  char *a1, *a2;
+
+  for ( size_t i = 0; i < aut_descriptor_get_num_transitions(d1); ++i) {
+    t1 = aut_get_transition(lts1, i);
+    t2 = aut_get_transition(lts2, i);
+
+    ASSERT_EQ(aut_transition_get_source(t1), aut_transition_get_source(t2));
+    ASSERT_EQ(aut_transition_get_target(t1), aut_transition_get_target(t2));
+    err = aut_transition_get_action(t1, &a1);
+    ASSERT_EQ(AUT_ESUCCESS, err);
+    err = aut_transition_get_action(t2, &a2);
+    ASSERT_EQ(AUT_ESUCCESS, err);
+    ASSERT_EQ( 0, strcmp(a1, a2));
+  }
+
+  remove(OUTFILE);
 }
 
 
